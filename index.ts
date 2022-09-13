f58de272e9de91d66dfb652e6eacaa32eff7ca5f
Mohammad Ahmad <mohammad.ahmad@carbonteq.com>
2022-09-13 16:03:06 +0500

    further additions of functionality

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 console.log("This is just the beginning my dudes");
+
+import { TodoList } from "./Domain/Aggregates/List";
+
+const myList = new TodoList();
+const date = new Date(Date.now());
+myList.AddNewTask("help", "dfadsf", new Date(Date.now() + 100000000));
+myList.AddNewTask("help", "dfadsf", new Date(Date.now() + 100000000));
+myList.AddNewTask("help", "dfadsf", new Date(Date.now() + 100000000));
+
+console.log(myList.getList());
