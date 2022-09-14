ae36f6a5b01204bd01d6ae4aaa87851246a39cd0
Mohammad Ahmad <mohammad.ahmad@carbonteq.com>
2022-09-14 15:06:06 +0500

    setup user entity, email vlaue object and persistance

---
+++ Domain/Entities/User.entity.ts
@@ -0,0 +1,32 @@
+import { Email } from "../ValueObjects/Email.obj";
+import { EntityBase } from "./EntityBase";
+
+export class User extends EntityBase {
+  private _Username: string;
+  private _Password: string;
+  private _Email: Email;
+
+  constructor(_Username: string, _Password: string, _Email: string) {
+    super();
+
+    this._Username = _Username;
+    this._Password = _Password;
+    this._Email = new Email(_Email);
+  }
+
+  get Username(): string {
+    return this._Username;
+  }
+
+  get Password(): string {
+    return this._Password;
+  }
+
+  set Password(_Password: string) {
+    this._Password = _Password;
+  }
+
+  get Email(): string {
+    return this._Email.value;
+  }
+}
